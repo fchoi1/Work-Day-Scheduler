2e64ffd95ce32844ac59f7e9b541d1224b3d707e
fchoi1 <masterfabster2010@hotmail.com>
2021-11-19 21:25:22 -0500

    added saving tasks for future/prevvious days

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Work Day Scheduler Starter Code
+Challenge 5 Using Third Party API
+
+## Website
+
+[Organize your day here!](https://fchoi1.github.io/Work-Day-Scheduler/)
+
+## Technologies
+Made using HTML, CSS, and Javascript with a focus on Bootstrap, JQuery
+
+<img src="./assets/images/website_screenshot.PNG" width="500">
+
+## How to use the password generator
+
+1. The current day is displayed:
+
+    <img src="./assets/images/step1.png" width="500">
+
+
+2. Click on a text area in a time slot to input a new event;
+
+    * Grey timeblocks meaning that the event has past
+
+    * Red timeblocks are events occuring now
+
+    * Green timeblocks are events in the future
+
+    <img src="./assets/images/step2.png" width="600">
+
+3. Press the blue save button on the right to save the event:
+
+    Series of prompts to select which types of characters to use:
+
+    <img src="./assets/images/step3.png" width="100">
+
+
+4. You can edit previous tasks buy simply clicking on them! (Don't forget to save changed tasks!)
+
+    <img src="./assets/images/step4.png" width="600">
+
+4. You can remove tasks by clearing the event info! (Don't forget to save changed tasks!)
+
+    <img src="./assets/images/step5a.png" width="600">
+    <img src="./assets/images/step5b.png" width="600">
+
+
+
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var currentDate = moment().format('LL')
 var currentDateString = moment().format("dddd MMMM Do YYYY");
 
+// Array is designed to handle multiple days!!!s
 var dailytask = []; // { date: [time: [ taskName:   ]]} 
 
 var workHours = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM']; // Create array for work hours
@@ -40,60 +41,72 @@ var createTimeBlock = function(){
         saveEventEl.html("<i class='bi bi-save'></i>");
 
         timeBlockEl.append(timeBlockTitleEl,eventInfoBlockEl,saveEventEl);
-        updateTimeBlock(timeBlockEl);   
         timeBlcokContainerEl.append(timeBlockEl);
     }
 }
 
 //Update time block colours based on time
 var updateTimeBlock = function(timeBlockEl){
-    var currentHour =  moment().format("hh a"); // Display the same as work hour time
-    
-    var timeBlockdate = timeBlockEl.data("date");
-    var timeBlockHour = timeBlockEl.data("hour");;
+    var timeBlockdate = timeBlockEl.attr("data-date");
+    var timeBlockHour = timeBlockEl.attr("data-hour");
     var momentTimeBlock = moment(timeBlockdate + " " + timeBlockHour, "MMMM Do YYYY h A");
-    var currentDateTime = moment("Friday November 19th 2021 1 PM", "dddd MMMM Do YYYY h A"); //.format(" MMMM Do YYYY hh a")
+    var currentDateTime = moment(); //.format(" MMMM Do YYYY hh a")
     // console.log(currentDateTime,timeBlockDate,momentTimeBlock);
-
+    console.log("date to compare", timeBlockdate, timeBlockEl)
     // Check if time passed, coming up, or in the future
     if (moment(currentDateTime).isAfter(momentTimeBlock)){
-        console.log("time passed, current: " + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
-        timeBlockEl.toggleClass('past');
+        //console.log("time passed, current: " + moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+        timeBlockEl.removeClass('present future');
+        timeBlockEl.addClass('past');
     }else if(Math.abs(moment(currentDateTime).diff(momentTimeBlock, "hours")) < 1){
-        console.log("within an hour");
-        timeBlockEl.toggleClass('present');
+        //console.log("within an hour");
+        timeBlockEl.removeClass('past future');
+        timeBlockEl.addClass('present');
     }else{
-        console.log("not passed");
-        timeBlockEl.toggleClass('future');
+        //console.log("not passed");
+        timeBlockEl.removeClass('present past');
+        timeBlockEl.addClass('future');
     }
-
 }
 
 // Used to create events
-var createEventTasks = function(dailytaskArray){
-    // Get current date and display only for current day events
-    for (time in dailytaskArray[currentDate]){
-        var timeObj = dailytaskArray[currentDate][time];
+var createEventTasks = function(dailytaskArray,useDate=null){
+    // clear child elements and re-create them except for text area
+    $(".event-info-area").find('*').not('.create-text-area').remove();
 
+    var currDate = '';
+    if(!useDate){
+        currDate = currentDate;
+    }else{
+        currDate = useDate;
+    }
+    // Get current date and display only for current day events
+    for (time in dailytaskArray[currDate]){
+        var timeObj = dailytaskArray[currDate][time];
+        //console.log("selector: ", ".time-block[data-hour='"  + time + "'][data-date='" + currentDate +"'] ");
+        var currentTimeBlockEl = $(".time-block[data-hour='"  + time + "'][data-date='" + currentDate +"'] ");
+        // Update color
+        console.log("update color", currentTimeBlockEl);
+
+        updateTimeBlock(currentTimeBlockEl);
         if(timeObj.length !== 0){ // If there tasks at that hour
             // Find correct time block text area based on time
-            let timeBlockTextAreaEl = $(".time-block[data-hour='"  + time + "'] ")
-                .children(".event-info-area")
-                .children(".event-text-area"); 
+            let timeBlockTextAreaEl = currentTimeBlockEl.children(".event-info-area").children(".event-text-area");                 
             for(task of timeObj){
                 // Create paragraph element
                 let taskPEl = $("<p>").addClass(paragraphClass).text(task);
                 timeBlockTextAreaEl.before(taskPEl);
             }
         }
-    }     
+    } 
+    console.log("Successfull updated tasks from array"); 
 }
 
 // Saving tasks 
 var saveButtonHandler = function(){
     let timeBlockEl = $(this).parent();
-    let hour = timeBlockEl.data('hour');
-    let date = timeBlockEl.data('date');
+    let hour = timeBlockEl.attr('data-hour');
+    let date = timeBlockEl.attr('data-date');
     // Get list of text area to be updated, this means it is edited or new
     var eventTextAreas = timeBlockEl.find(".event-text-area, .event-text");
     dailytask[date][hour] = []; // Reset the event tasks, full list updated every save, easier
@@ -124,7 +137,6 @@ var saveButtonHandler = function(){
         }
         
     });
-
     saveEvents();
     console.log("save tasks", dailytask);
 }
@@ -162,16 +174,55 @@ var editTaskHandler = function(){
     textInput.focus();
 }
 
+var updateTimeBlockDate = function(date){
+    // update date in data set
+    $(".container").find(".time-block").each(function(index, el){
+        $(this).attr('data-date', date);
+    });
+}
+
 // Used to update and color code tasks every 30 mins
 setInterval(function(){
     $(".time-block").each(function(index,el){
-        updateTimeBlock(el);
+        updateTimeBlock($(this));
     });
   }, (1000 * 60) *30); // 30 mins in seconds
 
+// Next Day Tasks
+$("#nextDay").on("click", function(){
+    // add a day to currentDate and Date string
+
+    currentDate = moment(currentDate, 'LL').add(1,'d').format('LL');
+    currentDateString = moment(currentDateString, '"dddd MMMM Do YYYY"')
+        .add(1,'d')
+        .format("dddd MMMM Do YYYY");
+    // Create new array if new it doesn't exist:
+    if(!dailytask[currentDate] ){
+        dailytask[currentDate] = {};
+        for (hour of workHours){  
+            dailytask[currentDate][hour] = [];
+        }
+        console.log("No entry exist, need to create")
+    }
+    updateTimeBlockDate(currentDate);
+    $("#currentDay").text(currentDateString); // Update current date
+    createEventTasks(dailytask);
+});
+
+// Previous Day Tasks
+$("#previousDay").on("click", function(){
+    // suibtract a day to currentDate
+    currentDate = moment(currentDate, 'LL').subtract(1,'d').format('LL');
+    currentDateString = moment(currentDateString, '"dddd MMMM Do YYYY"')
+        .subtract(1,'d')
+        .format("dddd MMMM Do YYYY");
+    updateTimeBlockDate(currentDate);
+    $("#currentDay").text(currentDateString); // Update current date
+    createEventTasks(dailytask);
+});
+
 // Allow to edit tasks
 $(".container").on("click", ".event-text", editTaskHandler);
-
 
 // Saving new/edited events button
 $(".container").on("click", ".saveBtn", saveButtonHandler);
